71828de9574c993b34f9a9ed5e367a2e5457a1c3
Tai Ha <u7454578@anu.edu.au>
2023-06-16 22:50:45 +1000

    Adding spring security and CORS

---
+++ src/main/java/com/StudyMate/StudyMate/sercurity/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.StudyMate.StudyMate.sercurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    /**
+     * TODO: the current authorisation rules are temporary (it accepts all paths), update it later to require authorisation
+     * to some paths
+     */
+    @Bean
+    SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/**").permitAll()
+                );
+        return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
