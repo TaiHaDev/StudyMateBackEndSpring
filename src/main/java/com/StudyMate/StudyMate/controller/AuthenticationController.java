b624efbfb8b346b99d5f38c359bdbfed13cceb8b
Tai Ha <u7454578@anu.edu.au>
2023-07-06 20:42:03 +1000

    AuthenticationController.java

--- src/main/java/com/StudyMate/StudyMate/controller/AuthenticationController.java
+++ src/main/java/com/StudyMate/StudyMate/controller/AuthenticationController.java
@@ -1,4 +1,16 @@
 package com.StudyMate.StudyMate.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthenticationController {
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser() {
+
+    }
 }
